88322fef6498b9d70c8cf6fff4e76b334e723494
Peculiar07 <peculiarfawole54@gmail.com>
2025-07-05 07:08:01 +0100

    Add files via upload

---
+++ UNIQUE CATERIN/js/script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('thank-you').style.display = 'block';
+    this.reset();
+});
